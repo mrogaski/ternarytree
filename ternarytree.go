dddec6444e2ccb162817c3103e7139feae8d66d6
Mark Rogaski <mrogaski@pobox.com>
2019-03-03 01:25:13 -0500

    Added Traverse and Sort (#6)

--- ternarytree.go
+++ ternarytree.go
@@ -68,6 +68,40 @@ func searchVisitor(node *treeNode, head byte, tail string) bool {
 	}
 }
 
+// Traverse recursively applies a function to all stored strings.
+func (tree *TernaryTree) Traverse(f func(string)) {
+	if tree.hasEmpty {
+		f("")
+	}
+	traverseVisitor(tree.head, f)
+}
+
+func traverseVisitor(node *treeNode, f func(string)) {
+	if node == nil {
+		return
+	}
+	traverseVisitor(node.loKid, f)
+	traverseVisitor(node.eqKid, f)
+	if node.data != nil {
+		f(*node.data)
+	}
+	traverseVisitor(node.hiKid, f)
+
+}
+
+// Sort returns a sorted list of strings contained in the tree.
+func (tree *TernaryTree) Sort() []string {
+	var result []string
+	collector := func(s string) {
+		result = append(result, s)
+	}
+	if tree.hasEmpty {
+		result = append(result, "")
+	}
+	traverseVisitor(tree.head, collector)
+	return result
+}
+
 // PartialMatchSearch finds matches for wildcard patterns in the ternary search tree.
 func (tree *TernaryTree) PartialMatchSearch(s string, wildcard byte) []string {
 	var result []string
